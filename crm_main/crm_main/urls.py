eb38985476986f7a52e68cd6c1b30c93df087955
DavidLeeBlue <davidleecs@outlook.com>
2024-02-28 17:42:52 +0000

    dashboard

--- crm_main/crm_main/urls.py
+++ crm_main/crm_main/urls.py
@@ -1,13 +1,18 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views
+from django.urls import path, include
 from core.views import index
 from userprofile.views import signup
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
-    path('signup/', signup, name='signup'),
+    path('dashboard/', include('dashboard.urls')),
+    path('sign-up/', signup, name='signup'),
+    path('log-in/',views.LoginView.as_view(template_name='userprofile/login.html') ,name='login'),
+    path('log-out/',views.LogoutView.as_view() ,name='logout'),
+  
 ]
 
 """
--- crm_main/userprofile/views.py
+++ crm_main/userprofile/views.py
@@ -1,9 +1,20 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .models import Userprofile
 
 # from team.models import Team
 
 def signup(request):
-    form = UserCreationForm()
-    return render(request, 'userprofile/signup.html')
+    if request.method  == 'POST':
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            userprofile = Userprofile.objects.create(user=user)
+            return redirect('log-in')
+    else:
+        form = UserCreationForm()
+    return render(request, 'userprofile/signup.html',{
+        'form':form
+    })
