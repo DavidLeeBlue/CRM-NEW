71dd7a8b04d3b64b5581c106ada4706e1c8bbfd2
DavidLeeBlue <davidleecs@outlook.com>
2024-02-29 21:13:52 +0000

    update clients

--- crm_main/client/urls.py
+++ crm_main/client/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.clients_list, name='clients_list'),
     path('<int:pk>/', views.client_detail, name='clients_detail'),
+    path('add/', views.client_add, name='clients_add'),
 ]
--- crm_main/client/views.py
+++ crm_main/client/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Client
+from .forms import AddClientForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -14,3 +16,20 @@ def clients_list(request):
 def client_detail(request, pk):
     client = get_object_or_404(Client,created_by=request.user, pk=pk)
     return render(request, 'client/clients_detail.html', {'client': client})
+
+
+@login_required
+def client_add(request):
+    if request.method == 'POST':
+        form = AddClientForm(request.POST)
+        if form.is_valid():
+            lead = form.save(commit=False) # don't save the form yet, need to add created_by
+            lead.created_by = request.user
+            lead.save()
+            messages.success(request, 'The lead has been added successfully!')
+            return redirect('clients_list')
+    else:
+        form = AddClientForm()
+    return render(request, 'client/clients_add.html',{
+        'form': form
+    }) # one slash here.
