3fd29bf0d57895ed5a30ae5db9cdc12f6ab7a1f1
DavidLeeBlue <davidleecs@outlook.com>
2024-02-29 20:15:42 +0000

    display client details

--- crm_main/client/views.py
+++ crm_main/client/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Client
 
@@ -7,4 +7,10 @@ from .models import Client
 @login_required
 def clients_list(request):
     clients = Client.objects.filter(created_by=request.user)
+    # clients = get_object_or_404(Client, created_by=request.user)
     return render(request, 'client/clients_list.html',{ 'clients': clients })
+
+@login_required
+def client_detail(request, pk):
+    client = get_object_or_404(Client,created_by=request.user, pk=pk)
+    return render(request, 'client/clients_detail.html', {'client': client})
