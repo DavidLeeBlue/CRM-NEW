a2e4070d0adb73eb8c0ff899b957e3e8d1d2f0e9
DavidLeeBlue <davidleecs@outlook.com>
2024-02-29 19:31:40 +0000

    display clients list

--- crm_main/crm_main/urls.py
+++ crm_main/crm_main/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('dashboard/leads/', include('lead.urls')),
+    path('dashboard/clients/', include('client.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('sign-up/', signup, name='signup'),
     path('log-in/',views.LoginView.as_view(template_name='userprofile/login.html') ,name='login'),
