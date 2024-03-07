39cf708e234885c37d1fd08498d18ba20f6309d6
DavidLeeBlue <davidleecs@outlook.com>
2024-03-07 21:26:24 +0000

    display products

--- crm_main/product/models.py
+++ crm_main/product/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    # description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock_quantity = models.IntegerField()
+    created_by = models.ForeignKey(User, related_name='products', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
