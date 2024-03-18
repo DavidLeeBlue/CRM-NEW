224707657d585258ea9e337355992097673bcde2
DavidLeeBlue <davidleecs@outlook.com>
2024-03-18 21:49:57 +0000

    add order_product table

--- crm_main/order/models.py
+++ crm_main/order/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from client.models import Client
+from product.models import Product
 
 # Create your models here.
 
@@ -31,3 +32,20 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.order_number}"
+    
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, related_name='order_products', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_products', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    order_date = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.unit_price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.quantity}"
