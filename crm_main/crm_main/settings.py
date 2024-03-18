15d69f75d1bc838d847972054306019082f2756d
DavidLeeBlue <davidleecs@outlook.com>
2024-03-18 17:11:50 +0000

    add breadcrumb

--- crm_main/crm_main/settings.py
+++ crm_main/crm_main/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'dashboard',
     'product',
     'order',
+    'dynamic_breadcrumbs',
 ]
 
 MIDDLEWARE = [
@@ -73,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'dynamic_breadcrumbs.context_processors.breadcrumbs',
             ],
         },
     },
